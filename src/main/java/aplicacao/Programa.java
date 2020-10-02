cc40c2689f7fa6de63473dab11b4adb59d2b6dba
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-10-01 21:46:16 -0300

    Adicionado projeto

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,28 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Terminado!");
+		em.close();
+		emf.close();
+		
+	}
+
+}
